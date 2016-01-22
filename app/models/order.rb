170f838814175d740bb2680cb79fb3f9e54ebc07
Brian Bensch <bbensch@gmail.com>
2016-01-22 13:41:33 -0800

    cart add/remove/view all working with normal routes

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,12 @@ class Order < ActiveRecord::Base
   has_many :shopping_cart_items
   belongs_to :user
   belongs_to :payment_info
+
+  def order_total
+    sum = 0
+    self.shopping_cart_items.each do |item|
+      sum += item.product.price * item.quantity
+    end
+    sum
+  end
 end
