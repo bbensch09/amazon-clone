9948867dfac808e95f2c19814c99892596aabbc6
Abe Clark <email": null>
2016-01-22 15:17:01 -0800

    Checkout flow generally working

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -8,6 +8,13 @@ class CheckoutController < ApplicationController
   end
 
   def save_checkout
+    @user = current_user
+    @order = @user.orders.find_by(status: 'in_cart')
+    # @order.update(status: 'ordered')
+    # @products = @order.shopping_cart_items
+    @address = @order.address if @order
+    @payment_info = @order.payment_info if @order
 
+    render 'success'
   end
 end
